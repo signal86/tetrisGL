21202b4e91934c52b684ac656faafe576111a64f
signal86 <signal86@github.com>
2024-01-24 09:05:38 -0600

    orthographic projection

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,14 @@
 #include <sstream>
 #include <vector>
 #include "shader.h"
+#define NK_IMPLEMENTATION
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#include <nuklear/nuklear.h>
 
 
 static void keyHandler(GLFWwindow *window, int key, int scancode, int action, int mods) {
@@ -23,18 +31,6 @@ static void keyHandler(GLFWwindow *window, int key, int scancode, int action, in
 
             case GLFW_KEY_ESCAPE:
                 glfwSetWindowShouldClose(window, GLFW_TRUE);
-                break;
-
-            case GLFW_KEY_R:
-                glClearColor(1, 0, 0, 1.0);
-                break;
-
-            case GLFW_KEY_G:
-                glClearColor(0, 1, 0, 1.0);
-                break;
-
-            case GLFW_KEY_B:
-                glClearColor(0, 0, 1, 1.0);
                 break;
 
         }
@@ -78,11 +74,12 @@ int main() {
     glm::mat4 Projection = glm::perspective(glm::radians(45.0f), (float) 1280 / (float) 720, 0.1f, 100.0f);
 
     // Camera matrix
-    glm::mat4 View = glm::lookAt(
-        glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
-        glm::vec3(0,0,0), // and looks at the origin
-        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-    );
+    // glm::mat4 View = glm::lookAt(
+    //     glm::vec3(0,0,1), // Camera is at (4,3,3), in World Space
+    //     glm::vec3(0,0,0), // and looks at the origin
+    //     glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+    // );
+    glm::mat4 View = glm::ortho(0.0f, 1280.0f, 0.0f, 720.0f, -1.0f, 100.0f);
 
     // Model/identity matrix
     glm::mat4 Model = glm::mat4(1.0f);
@@ -97,92 +94,59 @@ int main() {
     glm::mat4 mvp = Projection * View * Model; // Has to be multiplied in order
 
     static const GLfloat g_vertex_buffer_data[] = {
-        -1.0f,-1.0f,-1.0f, // triangle 1 : begin
-        -1.0f,-1.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f, // triangle 1 : end
-        1.0f, 1.0f,-1.0f, // triangle 2 : begin
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f,-1.0f, // triangle 2 : end
-        1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f, 1.0f,
-        -1.0f,-1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        -1.0f,-1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f,-1.0f,
-        1.0f,-1.0f,-1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f,-1.0f,
-        -1.0f, 1.0f,-1.0f,
-        1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f,-1.0f,
-        -1.0f, 1.0f, 1.0f,
-        1.0f, 1.0f, 1.0f,
-        -1.0f, 1.0f, 1.0f,
-        1.0f,-1.0f, 1.0f
+        0.0f, 720.0f, 0.0f,
+        640.0f, 0.0f, 0.0f,
+        1280.0f, 720.0f, 0.0f
     };
 
-    static const GLfloat g_color_buffer_data[] = {
-        0.583f,  0.771f,  0.014f,
-        0.609f,  0.115f,  0.436f,
-        0.327f,  0.483f,  0.844f,
-        0.822f,  0.569f,  0.201f,
-        0.435f,  0.602f,  0.223f,
-        0.310f,  0.747f,  0.185f,
-        0.597f,  0.770f,  0.761f,
-        0.559f,  0.436f,  0.730f,
-        0.359f,  0.583f,  0.152f,
-        0.483f,  0.596f,  0.789f,
-        0.559f,  0.861f,  0.639f,
-        0.195f,  0.548f,  0.859f,
-        0.014f,  0.184f,  0.576f,
-        0.771f,  0.328f,  0.970f,
-        0.406f,  0.615f,  0.116f,
-        0.676f,  0.977f,  0.133f,
-        0.971f,  0.572f,  0.833f,
-        0.140f,  0.616f,  0.489f,
-        0.997f,  0.513f,  0.064f,
-        0.945f,  0.719f,  0.592f,
-        0.543f,  0.021f,  0.978f,
-        0.279f,  0.317f,  0.505f,
-        0.167f,  0.620f,  0.077f,
-        0.347f,  0.857f,  0.137f,
-        0.055f,  0.953f,  0.042f,
-        0.714f,  0.505f,  0.345f,
-        0.783f,  0.290f,  0.734f,
-        0.722f,  0.645f,  0.174f,
-        0.302f,  0.455f,  0.848f,
-        0.225f,  0.587f,  0.040f,
-        0.517f,  0.713f,  0.338f,
-        0.053f,  0.959f,  0.120f,
-        0.393f,  0.621f,  0.362f,
-        0.673f,  0.211f,  0.457f,
-        0.820f,  0.883f,  0.371f,
-        0.982f,  0.099f,  0.879f
-    };
+    // static const GLfloat g_color_buffer_data[] = {
+    //     0.583f,  0.771f,  0.014f,
+    //     0.609f,  0.115f,  0.436f,
+    //     0.327f,  0.483f,  0.844f,
+    //     0.822f,  0.569f,  0.201f,
+    //     0.435f,  0.602f,  0.223f,
+    //     0.310f,  0.747f,  0.185f,
+    //     0.597f,  0.770f,  0.761f,
+    //     0.559f,  0.436f,  0.730f,
+    //     0.359f,  0.583f,  0.152f,
+    //     0.483f,  0.596f,  0.789f,
+    //     0.559f,  0.861f,  0.639f,
+    //     0.195f,  0.548f,  0.859f,
+    //     0.014f,  0.184f,  0.576f,
+    //     0.771f,  0.328f,  0.970f,
+    //     0.406f,  0.615f,  0.116f,
+    //     0.676f,  0.977f,  0.133f,
+    //     0.971f,  0.572f,  0.833f,
+    //     0.140f,  0.616f,  0.489f,
+    //     0.997f,  0.513f,  0.064f,
+    //     0.945f,  0.719f,  0.592f,
+    //     0.543f,  0.021f,  0.978f,
+    //     0.279f,  0.317f,  0.505f,
+    //     0.167f,  0.620f,  0.077f,
+    //     0.347f,  0.857f,  0.137f,
+    //     0.055f,  0.953f,  0.042f,
+    //     0.714f,  0.505f,  0.345f,
+    //     0.783f,  0.290f,  0.734f,
+    //     0.722f,  0.645f,  0.174f,
+    //     0.302f,  0.455f,  0.848f,
+    //     0.225f,  0.587f,  0.040f,
+    //     0.517f,  0.713f,  0.338f,
+    //     0.053f,  0.959f,  0.120f,
+    //     0.393f,  0.621f,  0.362f,
+    //     0.673f,  0.211f,  0.457f,
+    //     0.820f,  0.883f,  0.371f,
+    //     0.982f,  0.099f,  0.879f
+    // };
 
 	GLuint vertexBuffer;
 	glGenBuffers(1, &vertexBuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
-	GLuint colorBuffer;
-	glGenBuffers(1, &colorBuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
+	// GLuint colorBuffer;
+	// glGenBuffers(1, &colorBuffer);
+	// glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+	// glBufferData(GL_ARRAY_BUFFER, sizeof(g_color_buffer_data), g_color_buffer_data, GL_STATIC_DRAW);
 
     do {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -205,22 +169,22 @@ int main() {
 			(void*)0            // array buffer offset
         );
 
-        glEnableVertexAttribArray(1);
-        glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
-        glVertexAttribPointer(
-			1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
-			3,                  // size
-			GL_FLOAT,           // type
-			GL_FALSE,           // normalized?
-			0,                  // stride
-			(void*)0            // array buffer offset
-        );
+        // glEnableVertexAttribArray(1);
+        // glBindBuffer(GL_ARRAY_BUFFER, colorBuffer);
+        // glVertexAttribPointer(
+		// 	1,                  // attribute 1. No particular reason for 1, but must match the layout in the shader.
+		// 	3,                  // size
+		// 	GL_FLOAT,           // type
+		// 	GL_FALSE,           // normalized?
+		// 	0,                  // stride
+		// 	(void*)0            // array buffer offset
+        // );
 
 
-        glDrawArrays(GL_TRIANGLES, 0, 12*3);
+        glDrawArrays(GL_TRIANGLES, 0, 1*3);
 
         glDisableVertexAttribArray(0);
-        glDisableVertexAttribArray(1);
+        // glDisableVertexAttribArray(1);
 
         glfwSwapBuffers(window);
     } while (!glfwWindowShouldClose(window));
